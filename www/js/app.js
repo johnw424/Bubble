7e5d77ea987e778d4743676835715dd10e0cd112
John Wang <johnw424@gmail.com>
2014-07-07 16:32:47 -0700

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,37 @@
+angular.module('chatRoom', ['ionic', 'chatRoom.services', 'chatRoom.filters', 'chatRoom.controllers', 'firebase'])
+
+.config(function ($compileProvider){
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('Home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'RoomListCtrl'
+  });
+
+  $stateProvider.state('roomsId', {
+    url: '/rooms/:roomId',
+    templateUrl: 'templates/room.html',
+    controller: 'RoomCtrl'
+  });
+
+  $stateProvider.state('newRoom', {
+    url: '/new',
+    templateUrl: 'templates/new_room.html',
+    controller: 'RoomCreateCtrl'
+  });
+
+  $stateProvider.state('about', {
+    url: '/about',
+    templateUrl: 'templates/about.html',
+    controller: 'AboutCtrl'
+  });
+
+  
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
+
+});
+
